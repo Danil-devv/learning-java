df470cc9a4a857f12366925e05c9942105927c1f
Danil <s3menovd4nil@yandex.ru>
2022-11-15 01:46:41 +0300

    added new methods

--- src/Vector2D.java
+++ src/Vector2D.java
@@ -21,4 +21,23 @@ public class Vector2D {
     public void print() {
         System.out.println(String.format(Locale.US, "(%.2f, %.2f)", this.vX, this.vY));
     }
+
+    public double length() {
+        return Math.sqrt(this.vX * this.vX + this.vY * this.vY);
+    }
+
+    public void add(Vector2D v) {
+        this.vX += v.vX;
+        this.vY += v.vY;
+    }
+
+    public void sub(Vector2D v) {
+        this.vX -= v.vX;
+        this.vY -= v.vY;
+    }
+
+    public void scale(double scaleFactor) {
+        this.vX *= scaleFactor;
+        this.vY *= scaleFactor;
+    }
 }
