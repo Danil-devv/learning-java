00c9e5c432f6f9ac8ec1f9cb94d5b91534390544
Danil <s3menovd4nil@yandex.ru>
2022-11-15 01:57:08 +0300

    added new methods: normalized() and dotProduct() also added a new static field

--- src/Vector2D.java
+++ src/Vector2D.java
@@ -1,19 +1,21 @@
 import java.util.Locale;
 
 public class Vector2D {
-    public double vX, vY;
+    public double vX = 1, vY = 1;
+    public static int count = 0;
 
     public Vector2D() {
-        this.vX = 1;
-        this.vY = 1;
+        count++;
     }
 
     public Vector2D(double x, double y) {
+        this();
         this.vX = x;
         this.vY = y;
     }
 
     public Vector2D(Vector2D v) {
+        this();
         this.vX = v.vX;
         this.vY = v.vY;
     }
@@ -39,5 +41,15 @@ public class Vector2D {
     public void scale(double scaleFactor) {
         this.vX *= scaleFactor;
         this.vY *= scaleFactor;
+    }
+
+    public void normalized() {
+        double length = this.length();
+        this.vX /= length;
+        this.vY /= length;
+    }
+
+    public double dotProduct(Vector2D v) {
+        return this.vX * v.vX + this.vY * v.vY;
     }
 }
